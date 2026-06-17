d54a2c62c813a4fc870fb0378eca03d5bbb4424d
anhvuknguyen <anhvu.k.nguyen@gmail.com>
2026-06-17 23:30:47 +0900

    Added option menu; cleaned up main

--- src/main.cpp
+++ src/main.cpp
@@ -15,103 +15,158 @@ using namespace Cache_utils;
 
 namespace fs = std::filesystem;
 
+//Mapping Technique List
+Mapping_Technique MAPTECH[] = {Mapping_Technique::Direct,Mapping_Technique::Fully_Associative,Mapping_Technique::Set_Associative};
+string MAPTECH_strings[] = {"Direct","Fully Associative","Set Associative"};
+int MAPTECH_size = 3;
+
+//Replacement Policy List
+Replacement_Policy REPPOLICY[] = {Replacement_Policy::Direct,Replacement_Policy::LRU};
+string REPPOLICY_strings[] = {"Direct", "LRU"};
+int REPPOLICY_size = 2;
+
+//Cache Stats
+int setSize = 0;
+int numSets = 0;
+int numBlocks = 0;
+Mapping_Technique mapTech;
+Replacement_Policy repPolicy;
+Cache *cache = NULL;
+
+//Trace Directory
+string trace_directory = "./traces";
+
+Cache* buildCache(){
+    int mapTech_index = -1;
+    int repPolicy_index = -1;
+    do{
+        cout << "Choose the size of your set: ";
+        cin >> setSize;
+    }while(setSize==0 || !isPowerOfTwo(setSize));
+    do{
+        cout << "  Choose the number of sets: ";
+        cin >> numSets;
+    }while(numSets==0 || !isPowerOfTwo(numSets));
+    do{
+        cout << "Choose the number of blocks: ";
+        cin >> numBlocks;
+    }while(numBlocks==0 || !isPowerOfTwo(numBlocks));
+
+    do{
+        cout << " Choose a mapping technique: " << endl;
+        for(int i=0;i<MAPTECH_size;i++){
+            cout << "[" + to_string(i) + "] " + MAPTECH_strings[i] + "\n";
+        }
+        cout << ">> ";
+        cin >> mapTech_index;
+    }while(!(mapTech_index>-1 && mapTech_index<MAPTECH_size));
+    mapTech = MAPTECH[mapTech_index];
+    
+    do{
+        cout << "Choose a replacement policy: " << endl;
+        for(int i=0;i<REPPOLICY_size;i++){
+            cout << "[" + to_string(i) + "] " + REPPOLICY_strings[i] + "\n";
+        }
+        cout << ">> ";
+        cin >> repPolicy_index;
+    }while(!(repPolicy_index>-1 && repPolicy_index<REPPOLICY_size));
+    repPolicy = REPPOLICY[repPolicy_index];
+
+    return new Cache(setSize, numSets, numBlocks, mapTech, repPolicy);
+}
+
+void runTrace(){
+    string traceFile;
+    vector<string> *traceList = new vector<string>();
+    int traceSize = 0;
+    int traceIndex=-1;
+    cout << "Choose file from trace directory: " << endl;
+    try{
+        if(fs::exists(trace_directory) && fs::is_directory(trace_directory)){
+            do{
+                for (const auto& entry : fs::directory_iterator(trace_directory)) {
+                    traceList->push_back(entry.path());
+                    string str = entry.path();
+                    size_t substring_index = str.rfind("/");
+                    str = str.substr(substring_index+1);
+                    cout << "[" << to_string(traceSize) << "] " << str << endl;
+                    traceSize++;
+                }
+                cout << ">> ";
+                cin >> traceIndex;
+            }while(!(traceIndex>-1 && traceIndex<traceSize));
+        }
+    }
+    catch(const fs::filesystem_error& e){
+        cerr << "Error: " << e.what() << endl;
+    }
+    traceFile = traceList->at(traceIndex);
+    
+    ifstream file(traceFile);
+    if (!file.is_open()) {
+        cerr << "Failed to open file" << std::endl;
+        return;
+    }
+    string operation;
+    unsigned int address;
+    while (file.good()) {
+        file >> operation >> std::hex >> address;
+        cout << operation + " " + to_string(address) << endl;
+        Operation op = (operation=="R")? Operation::Read : Operation::Write;
+        cache->access(op,address);
+    }
+    cout << cache->getStats() << endl;
+    cout << cache->viewCache() << endl;
+}
+
+void clearCache(){
+    delete cache;
+    cache=NULL;
+}
+
 int main() {
     bool active = true;
-    Mapping_Technique MAPTECH[] = {Mapping_Technique::Direct,Mapping_Technique::Fully_Associative,Mapping_Technique::Set_Associative};
-    string MAPTECH_strings[] = {"Direct","Fully Associative","Set Associative"};
-    int MAPTECH_size = 3;
-    Replacement_Policy REPPOLICY[] = {Replacement_Policy::Direct,Replacement_Policy::LRU};
-    string REPPOLICY_strings[] = {"Direct", "LRU"};
-    int REPPOLICY_size = 2;
-
-    string trace_directory = "./traces";
+    
     while(active){
-        int setSize = 0;
-        int numSets = 0;
-        int numBlocks = 0;
-        Mapping_Technique mapTech;
-        Replacement_Policy repPolicy;
+        cout << "What would you like to do?" << endl;
+        cout << "[b] Build Cache" << endl;
+        cout << "[t] Trace File" << endl;
+        cout << "[c] Clear Cache" << endl;
+        cout << "[q] Quit" << endl;
 
-        int mapTech_index = -1;
-        int repPolicy_index = -1;
-        
-        do{
-            cout << "Choose the size of your set: ";
-            cin >> setSize;
-        }while(setSize==0 || !isPowerOfTwo(setSize));
-        do{
-            cout << "  Choose the number of sets: ";
-            cin >> numSets;
-        }while(numSets==0 || !isPowerOfTwo(numSets));
-        do{
-            cout << "Choose the number of blocks: ";
-            cin >> numBlocks;
-        }while(numBlocks==0 || !isPowerOfTwo(numBlocks));
+        string choice;
+        cout << ">> ";
+        cin >> choice;
 
-        do{
-            cout << " Choose a mapping technique: " << endl;
-            for(int i=0;i<MAPTECH_size;i++){
-                cout << "[" + to_string(i) + "] " + MAPTECH_strings[i] + "\n";
+        if(choice=="b"){
+            cache = buildCache();
+        }
+        else if(choice=="t"){
+            if(cache==NULL){
+                cout << "\nCache has not been built!\n" << endl;
+                continue;
             }
-            cout << ">> ";
-            cin >> mapTech_index;
-        }while(!(mapTech_index>-1 && mapTech_index<MAPTECH_size));
-        mapTech = MAPTECH[mapTech_index];
-        
-        do{
-            cout << "Choose a replacement policy: " << endl;
-            for(int i=0;i<REPPOLICY_size;i++){
-                cout << "[" + to_string(i) + "] " + REPPOLICY_strings[i] + "\n";
-            }
-            cout << ">> ";
-            cin >> repPolicy_index;
-        }while(!(repPolicy_index>-1 && repPolicy_index<REPPOLICY_size));
-        repPolicy = REPPOLICY[repPolicy_index];
-
-        Cache *cache = new Cache(setSize, numSets, numBlocks, mapTech, repPolicy);
-
-        string traceFile;
-        vector<string> *traceList = new vector<string>();
-        int traceSize = 0;
-        int traceIndex=-1;
-        cout << "Choose file from trace directory: " << endl;
-        try{
-            if(fs::exists(trace_directory) && fs::is_directory(trace_directory)){
-                do{
-                    for (const auto& entry : fs::directory_iterator(trace_directory)) {
-                        traceList->push_back(entry.path());
-                        string str = entry.path();
-                        size_t substring_index = str.rfind("/");
-                        str = str.substr(substring_index+1);
-                        cout << "[" << to_string(traceSize) << "] " << str << endl;
-                        traceSize++;
-                    }
-                    cout << ">> ";
-                    cin >> traceIndex;
-                }while(!(traceIndex>-1 && traceIndex<traceSize));
+            else{
+                runTrace();
             }
         }
-        catch(const fs::filesystem_error& e){
-            cerr << "Error: " << e.what() << endl;
+        else if(choice=="c"){
+            if(cache==NULL){
+                cout << "\nCache has not been built!\n" << endl;
+                continue;
+            }
+            else{
+                clearCache();
+            }
         }
-        traceFile = traceList->at(traceIndex);
+        else if(choice=="q"){
+            active=false;
+            cout << "Quitting" << endl;
+        }
+        else{
+            cout << "Invalid Option" << endl;
+        }
         
-        ifstream file(traceFile);
-        if (!file.is_open()) {
-            cerr << "Failed to open file" << std::endl;
-            return 1;
-        }
-        string operation;
-        unsigned int address;
-        while (file.good()) {
-            file >> operation >> std::hex >> address;
-            cout << operation + " " + to_string(address) << endl;
-            Operation op = (operation=="R")? Operation::Read : Operation::Write;
-            cache->access(op,address);
-        }
-        cout << cache->getStats() << endl;
-        cout << cache->viewCache() << endl;
-        
-        delete cache;
     }
 }
+
