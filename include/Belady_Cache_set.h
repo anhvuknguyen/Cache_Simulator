82c5e240c02c35ed80408e9e691accc9fc76c462
anhvuknguyen <anhvu.k.nguyen@gmail.com>
2026-07-17 23:41:09 +0900

    Began Belady Load file

--- include/Belady_Cache_set.h
+++ include/Belady_Cache_set.h
@@ -6,16 +6,19 @@
 #include "Types.h"
 #include <list>
 #include <unordered_map>
+#include <vector>
 
 class Belady_Cache_set: public Cache_set{
 private:
     std::list<Cache_line> *lineList;
     std::unordered_map<int,std::list<Cache_line>::iterator> *lineMap;
+    std::vector<pair<Cache_types::Operation,int>> *traceList;
 public:
     Belady_Cache_set(int setSize,Cache_types::Replacement_Policy repPolicy);
     ~Belady_Cache_set() override;
     std::string toString() override;
 
+    int loadTrace(std::string traceFile);
     Cache_types::Miss_Type lookup(int tag) override;
     int evict() override;
     int insert(int tag) override;
--- src/Belady_Cache_set.cpp
+++ src/Belady_Cache_set.cpp
@@ -2,22 +2,28 @@
 #include "Types.h"
 #include <cstdlib>
 #include <string> 
+#include <filesystem>
+#include <fstream>
 #include <stdexcept>
 #include <memory>
 #include <list>
 #include <unordered_map>
+#include <vector>
 
 using namespace std;
 using namespace Cache_types;
+namespace fs = std::filesystem;
 
 Belady_Cache_set::Belady_Cache_set(int setSize, Cache_types::Replacement_Policy repPolicy) : Cache_set(setSize,repPolicy){
     lineList = new list<Cache_line>();
     lineMap = new unordered_map<int,list<Cache_line>::iterator>();
+    traceList = new vector<pair<Operation,int>>();
 }
 
 Belady_Cache_set::~Belady_Cache_set() {
     delete lineList;
     delete lineMap;
+    delete traceList;
 }
 
 string Belady_Cache_set::toString() {
@@ -27,6 +33,19 @@ string Belady_Cache_set::toString() {
         str+=it->toString()+"\n";
     }
     return str;
+}
+
+int Belady_Cache_set::loadTrace(string traceFile){
+    ifstream file(traceFile);
+    if (!file.is_open()) {
+        return -1;
+    }
+    string operation;
+    unsigned int address;
+    while (file.good()) {
+        file >> operation >> std::hex >> address;
+        Operation op = (operation=="R")? Operation::Read : Operation::Write;
+    }
 }
 
 Miss_Type Belady_Cache_set::lookup(int tag){
